a29371334ed85b6f808d107b721fcade2fadb028
eva vargas <evargasabg@gmail.com>
2021-12-27 22:50:20 -0400

    Fix PORT 4

--- index.js
+++ index.js
@@ -17,34 +17,33 @@
 //     =====`-.____`.___ \_____/___.-`___.-'=====
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-const server = require('./src/app.js');
-const { conn } = require('./src/db');
-const axios = require('axios');
-const { Temper } = require('./src/db.js');
+const server = require("./src/app.js");
+const { conn } = require("./src/db");
+const axios = require("axios");
+const { Temper } = require("./src/db.js");
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
   async function getTempersFromApi() {
-    let result = await axios.get('https://api.thedogapi.com/v1/breeds');
-    const allTempers = result.data.map(dog => {
+    let result = await axios.get("https://api.thedogapi.com/v1/breeds");
+    const allTempers = result.data.map((dog) => {
       if (dog.temperament) {
-        const dogMap = dog.temperament.split(", ")
-        let temperArr = []
-        dogMap.map(temp => {
+        const dogMap = dog.temperament.split(", ");
+        let temperArr = [];
+        dogMap.map((temp) => {
           if (!temperArr.includes(temp)) {
-            temperArr.push(temp)
+            temperArr.push(temp);
           }
-        })
-        temperArr.map(tempN => {
-          Temper.findOrCreate({ where: { name: tempN } })
-        })
+        });
+        temperArr.map((tempN) => {
+          Temper.findOrCreate({ where: { name: tempN } });
+        });
       }
-    })
+    });
   }
-  getTempersFromApi()
+  getTempersFromApi();
 
-  server.listen(3002, () => {
-
-    console.log('%s listening at 3002'); // eslint-disable-line no-console
+  server.listen(process.env.PORT, () => {
+    console.log("%s listening at 3002"); // eslint-disable-line no-console
   });
 });
